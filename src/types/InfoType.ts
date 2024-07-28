67a0a4f4b45fb439d1e448b464e1f862dc3780ad
sadmanador <sadman.ador@gmail.com>
2024-07-28 19:55:27 +0600

    dark mode is done properly

--- src/types/InfoType.ts
+++ src/types/InfoType.ts
@@ -3,8 +3,8 @@ import { Dispatch, SetStateAction } from "react";
 type InfoContextProps = {
   sidebar: boolean;
   setSidebar: Dispatch<SetStateAction<boolean>>;
-  lightMode:boolean;
-   setLightMode: Dispatch<SetStateAction<boolean>>;
-}
+  lightMode: boolean;
+  setLightMode: Dispatch<SetStateAction<boolean>>;
+};
 
 export default InfoContextProps;
